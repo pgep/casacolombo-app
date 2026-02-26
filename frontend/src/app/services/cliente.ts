b3e3cb3834b312bfbeaae7e3fba2f0361e0a0668
Paulo Araujo <paulocoala@gmail.com>
2026-02-26 13:30:12 -0300

    Correção: adicionando frontend como arquivos reais (não submódulo)

---
+++ frontend/src/app/services/cliente.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Cliente {
+  id?: number;
+  nome: string;
+  email: string;
+  telefone: string;
+  ativo: boolean;
+  data_cadastro?: string;
+}
+
+@Injectable({
+  providedIn: 'root'  // Disponível em toda a aplicação
+})
+export class ClienteService {
+  private apiUrl = 'http://localhost:3001/api';
+
+  constructor(private http: HttpClient) { }
+
+  getClientes(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(`${this.apiUrl}/clientes`);
+  }
+
+  getCliente(id: number): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.apiUrl}/clientes/${id}`);
+  }
+
+  createCliente(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(`${this.apiUrl}/clientes`, cliente);
+  }
+
+  updateCliente(id: number, cliente: Cliente): Observable<Cliente> {
+    return this.http.put<Cliente>(`${this.apiUrl}/clientes/${id}`, cliente);
+  }
+
+  deleteCliente(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/clientes/${id}`);
+  }
+}
