9d658afd6938ffc5534859413558627bc36e3c82
Paulo Araujo <paulocoala@gmail.com>
2026-03-31 18:59:36 -0300

    Auste do menu

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { ImagemModalComponent } from './shared/components/imagem-modal/imagem-modal'; // ← IMPORT DO MODAL
+import { ImagemModalComponent } from './shared/components/imagem-modal/imagem-modal';
 import { ToastComponent } from './shared/components/toast/toast';
 import { ConfirmModalComponent } from './shared/components/confirm-modal/confirm-modal';
 
@@ -11,10 +11,10 @@ import { ConfirmModalComponent } from './shared/components/confirm-modal/confirm
   standalone: true,
   imports: [
     CommonModule,
-    RouterOutlet, // ← para o <router-outlet>
-    RouterModule, // ← para routerLink
+    RouterOutlet,
+    RouterModule,
     HttpClientModule,
-    ImagemModalComponent, // ← para chamadas HTTP
+    ImagemModalComponent,
     ToastComponent,
     ConfirmModalComponent,
   ],
@@ -23,26 +23,43 @@ import { ConfirmModalComponent } from './shared/components/confirm-modal/confirm
 })
 export class App {
   title = 'Sistema Casa Colombo Artesanal';
+
   // Controles do menu
   isMenuOpen = false;
   isCadastroOpen = false;
-  isTiposOpen = false;
+  isProducaoOpen = false;
+  isOperacionalOpen = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
     if (!this.isMenuOpen) {
       this.isCadastroOpen = false;
-      this.isTiposOpen = false;
+      this.isProducaoOpen = false;
+      this.isOperacionalOpen = false;
     }
   }
 
   toggleCadastro() {
     this.isCadastroOpen = !this.isCadastroOpen;
-    if (this.isCadastroOpen) this.isTiposOpen = false;
+    if (this.isCadastroOpen) {
+      this.isProducaoOpen = false;
+      this.isOperacionalOpen = false;
+    }
   }
 
-  toggleTipos() {
-    this.isTiposOpen = !this.isTiposOpen;
-    if (this.isTiposOpen) this.isCadastroOpen = false;
+  toggleProducao() {
+    this.isProducaoOpen = !this.isProducaoOpen;
+    if (this.isProducaoOpen) {
+      this.isCadastroOpen = false;
+      this.isOperacionalOpen = false;
+    }
+  }
+
+  toggleOperacional() {
+    this.isOperacionalOpen = !this.isOperacionalOpen;
+    if (this.isOperacionalOpen) {
+      this.isCadastroOpen = false;
+      this.isProducaoOpen = false;
+    }
   }
 }
